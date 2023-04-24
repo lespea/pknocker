27e5c2621495bf5ecf2d042ac9004ad74d2edcef
Adam Lesperance <lespea@gmail.com>
2023-04-24 00:41:45 -0500

    Working version

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,52 @@
 mod opts;
 
+use crate::opts::Target;
 use anyhow::Result;
 use clap::Parser;
+use env_logger::Env;
+use std::net::{SocketAddr, ToSocketAddrs};
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
+    // Setup logging
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    // Parse the args
     let cli = opts::Opts::parse();
+    let mut dst: SocketAddr = format!("{}:0", cli.dst)
+        .to_socket_addrs()
+        .expect("Bad dst")
+        .next()
+        .expect("No dst resolved");
 
-    println!("Hello, world!\n\n{cli:?}");
+    // If there is no global sleep wanted, don't bother with the logic
+    let do_sleep = cli.global_sleep > 0;
+
+    // Sleep to do
+    let gsleep = Target::Sleep {
+        secs: cli.global_sleep,
+    };
+
+    // If the current round should possibly insert a sleep statement.  Start
+    // with true so we don't do an initial sleep
+    let mut skip_sleep = true;
+
+    for action in cli.targets.iter() {
+        if do_sleep {
+            let is_sleep = action.is_sleep();
+
+            // Only insert a sleep if the last action wasn't a sleep (or start)
+            // and the current action isn't sleep
+            if is_sleep && !skip_sleep {
+                gsleep.run(&mut dst)
+            }
+
+            // Don't run sleep in the next round if we're currently doing a sleep
+            skip_sleep = !is_sleep;
+        }
+
+        // Run the current action
+        action.run(&mut dst)
+    }
+
     Ok(())
 }
--- src/opts.rs
+++ src/opts.rs
@@ -1,20 +1,29 @@
 use anyhow::{anyhow, Error, Result};
 use clap::Parser;
-use std::net::IpAddr;
+use log::info;
+use std::net::{Shutdown, SocketAddr, TcpStream, UdpSocket};
 use std::num::NonZeroU16;
 use std::str::FromStr;
+use std::thread::sleep;
+use std::time::Duration;
 
 #[derive(Parser, Clone, Debug, Eq, PartialEq)]
 #[clap(author, version, about)]
-pub(crate) struct Opts {
-    ip: IpAddr,
+pub struct Opts {
+    /// How long to sleep between each target
+    #[clap(long, default_value = "0")]
+    pub global_sleep: u8,
 
+    /// The IP to connect to
+    pub dst: String,
+
+    /// The list of target ports (or delays) to connect to
     #[clap(use_value_delimiter = true, required = true, value_parser = parse_target)]
-    targets: Vec<Target>,
+    pub targets: Vec<Target>,
 }
 
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub(crate) enum Target {
+pub enum Target {
     Tcp { port: NonZeroU16 },
     Udp { port: NonZeroU16 },
     Sleep { secs: u8 },
@@ -50,5 +59,46 @@ fn parse_target(value: &str) -> Result<Target> {
         }),
 
         ch => Err(anyhow!("Unknown ident '{ch}' for the value '{value}'")),
+    }
+}
+
+static CONN_TIMEOUT: Duration = Duration::from_secs(2);
+
+impl Target {
+    pub fn run(&self, dst: &mut SocketAddr) {
+        match self {
+            Self::Sleep { secs } => {
+                if *secs > 0 {
+                    let d = Duration::from_secs(*secs as u64);
+                    info!("Sleeping for {secs} seconds");
+                    sleep(d);
+                }
+            }
+
+            Self::Tcp { port } => {
+                dst.set_port(port.get());
+
+                info!("TCP {dst}");
+                let _ = TcpStream::connect_timeout(dst, CONN_TIMEOUT)
+                    .and_then(|s| s.shutdown(Shutdown::Both));
+            }
+
+            Self::Udp { port } => {
+                let baddr = if dst.is_ipv4() { "0.0.0.0:0" } else { "[::]:0" };
+
+                let sock = UdpSocket::bind(baddr).expect("Couldn't bind local udp sock");
+                sock.set_write_timeout(Some(CONN_TIMEOUT))
+                    .unwrap();
+
+                dst.set_port(port.get());
+                info!("UDP {dst}");
+                let _ = sock.send_to(&[], *dst);
+            }
+        };
+    }
+
+    #[inline]
+    pub fn is_sleep(&self) -> bool {
+        !matches!(self, Self::Sleep { .. })
     }
 }
